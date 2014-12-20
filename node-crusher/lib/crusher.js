1cd67925f8a1c4c239c31b0f5d1516cb9c842954
bergwerkgis <wb@bergwerk-gis.at>
2014-12-20 16:34:01 +0100

    allow to allocatore more than 1023MB

--- node-crusher/lib/crusher.js
+++ node-crusher/lib/crusher.js
@@ -1,52 +1,81 @@
 ﻿buf = require('buffer').Buffer;
+var path = require('path');
+var mname = module.filename.slice(__filename.lastIndexOf(path.sep) + 1, module.filename.length - 3);
 
-
+var MAX_BUF_SIZE_MB = 1023;
+var stop_eating_cpu = false;
 var buffer;
 var crusher = module.exports = exports;
 
 
 process.on('exit', function () {
-	if (buffer) {
-		buffer = null;
-	}
+	console.log(mname + ' exiting ...');
+	crusher.release_ram();
 });
 
 
 //bufferSize[MB]
 exports.eat_ram = function (bufferSize) {
-	if (bufferSize > 1023) {
-		console.log(bufferSize + ' is too big. for now ...');
-		return;
-	}
-	//TODO
-	//* array of buffers: MaxBufferSize=~1GB 0x3fffffff=1023MB: https://github.com/v8/v8/blob/c8bf5c35e431d4029e084024501863a4cf907882/src/objects.h#L4647-L4648
-	//* release allocation? how implement with array?
 	//IDEAS
 	//* param: array with time offsets and how much ram usage should increase at every time step
 	//* param: nTH call of 'eat_ram', add addtional buffer or replace current/latest
-	console.log('allocating ' + bufferSize + 'MB');
-	buffer = null; //release reference otherwise second call would increase used ram
-	buffer = new buf(bufferSize * 1024 * 1024);
-	buffer.fill('X');
+	this.release_ram(); //release reference otherwise second call would increase used ram
+
+	console.log(mname + ': allocating total of ' + bufferSize + ' MB (max buffer: ' + MAX_BUF_SIZE_MB + ' MB)');
+	if (bufferSize <= MAX_BUF_SIZE_MB) {
+		buffer = [];
+		buffer[0] = new buf(bufferSize * 1024 * 1024);
+		buffer[0].fill('X');
+	} else {
+		buffer = [];
+		var bufCnt = Math.floor(bufferSize / MAX_BUF_SIZE_MB) + 1;
+		console.log('bufCnt', bufCnt);
+		for (var i = 0; i < bufCnt; i++) {
+			var toAllocate = MAX_BUF_SIZE_MB;
+			if (0 === i) {
+				toAllocate = bufferSize % MAX_BUF_SIZE_MB
+			}
+			console.log(mname + ' allocating ' + toAllocate);
+			buffer[i] = new buf(toAllocate * 1024 * 1024);
+			buffer[i].fill('X');
+		}
+	}
 };
 
 
 exports.release_ram = function () {
 	if (buffer) {
-		console.log('releasing allocation reference...');
+		console.log(mname + ': releasing allocation reference...');
+		buffer.forEach(function (b) {
+			b = null;
+		});
 		buffer = null;
 	} else {
-		console.log('nothing allocated');
+		console.log(mname + ': nothing allocated');
 	}
 };
 
 
-exports.eat_cpu = function () {
+exports.eat_cpu = function (callback) {
 	//process.env.UV_THREADPOOL_SIZE = Math.ceil(Math.max(4, require('os').cpus().length * 1.5));
-	fibonacci(43);
+	stop_eating_cpu = false;
+	console.log(mname + ': eating cpu');
+	while (true !== stop_eating_cpu) {
+		fibonacci(43);
+	}
+	console.log(mname + ': eating cpu stopped');
+	callback(null, 'eating cpu stopped');
 };
 
+
+exports.stop_cpu = function (){
+	stop_eating_cpu = true;
+}
+
 function fibonacci(n) {
+	if (true === stop_eating_cpu) {
+		return 1;
+	}
 	if (n < 2) {
 		return 1;
 	} else {
--- node-crusher/test/test-ram.js
+++ node-crusher/test/test-ram.js
@@ -1,18 +1,22 @@
 ﻿var crusher = require('../');
+var path = require('path');
+var mname = module.filename.slice(__filename.lastIndexOf(path.sep) + 1, module.filename.length - 3);
 
-crusher.eat_ram(1024);
-
-console.log('before allocation: ', process.memoryUsage());
-crusher.eat_ram(500);
-console.log('after allocation: ', process.memoryUsage());
+crusher.eat_ram(1023);
+crusher.eat_ram(1025);
 
 setTimeout(function () {
-	crusher.release_ram();
-	crusher.eat_ram(950);
+	crusher.eat_ram(2500);
 	setTimeout(function () {
-		crusher.release_ram();
 		crusher.eat_ram(300);
-		crusher.eat_cpu();
+		console.log(mname + ': about to start cpu');
+		crusher.eat_cpu(function (err, msg) { console.log(mname + ': ' + msg); });
+		console.log(mname + ': started cpu');
+		setTimeout(function () {
+			console.log(mname + ': about to stop cpu');
+			crusher.stop_cpu();
+			console.log(mname + ': finished');
+		}, 3 * 1000);
 	}, 3 * 1000);
 }, 3 * 1000);
 
