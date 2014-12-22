f77a7841e33f198ef091c1eccf8b89069ea50fcd
bergwerkgis <wb@bergwerk-gis.at>
2014-12-22 12:51:11 +0100

    forgot test-cpu.js

---
+++ node-crusher/test/test-cpu.js
@@ -0,0 +1,10 @@
+﻿var crusher = require('../');
+var path = require('path');
+var mname = module.filename.slice(__filename.lastIndexOf(path.sep) + 1, module.filename.length - 3);
+
+crusher.eat_cpu();
+
+setTimeout(function () {
+	console.log(mname + ': about to stop cpu');
+	crusher.stop_cpu();
+}, 15 * 1000);
