2ec877580e19e08e3cbbae0bf28d081b5803071d
bergwerkgis <wb@bergwerk-gis.at>
2014-12-22 10:25:34 +0100

    fibonacci is now async. how to call it in an endless loop?

--- node-crusher/lib/crusher.js
+++ node-crusher/lib/crusher.js
@@ -1,8 +1,8 @@
-﻿buf = require('buffer').Buffer;
+﻿var Buffer = require('buffer').Buffer;
 var path = require('path');
 var mname = module.filename.slice(__filename.lastIndexOf(path.sep) + 1, module.filename.length - 3);
 
-var MAX_BUF_SIZE_MB = 1023;
+var MAX_BUF_SIZE_MB = 1023; //https://github.com/v8/v8/blob/c8bf5c35e431d4029e084024501863a4cf907882/src/objects.h#L4647-L4648
 var stop_eating_cpu = false;
 var buffer;
 var crusher = module.exports = exports;
@@ -20,62 +20,118 @@ exports.eat_ram = function (bufferSize) {
 	//* param: array with time offsets and how much ram usage should increase at every time step
 	//* param: nTH call of 'eat_ram', add addtional buffer or replace current/latest
 	this.release_ram(); //release reference otherwise second call would increase used ram
-
+	
 	console.log(mname + ': allocating total of ' + bufferSize + ' MB (max buffer: ' + MAX_BUF_SIZE_MB + ' MB)');
-	if (bufferSize <= MAX_BUF_SIZE_MB) {
-		buffer = [];
-		buffer[0] = new buf(bufferSize * 1024 * 1024);
-		buffer[0].fill('X');
-	} else {
-		buffer = [];
-		var bufCnt = Math.floor(bufferSize / MAX_BUF_SIZE_MB) + 1;
-		console.log('bufCnt', bufCnt);
-		for (var i = 0; i < bufCnt; i++) {
-			var toAllocate = MAX_BUF_SIZE_MB;
-			if (0 === i) {
-				toAllocate = bufferSize % MAX_BUF_SIZE_MB
+	try {
+		if (bufferSize <= MAX_BUF_SIZE_MB) {
+			buffer = [];
+			buffer[0] = new Buffer(bufferSize * 1024 * 1024);
+			buffer[0].fill('X');
+		} else {
+			buffer = [];
+			var bufCnt = Math.floor(bufferSize / MAX_BUF_SIZE_MB) + 1;
+			console.log('bufCnt', bufCnt);
+			for (var i = 0; i < bufCnt; i++) {
+				var toAllocate = MAX_BUF_SIZE_MB;
+				if (0 === i) {
+					toAllocate = bufferSize % MAX_BUF_SIZE_MB
+				}
+				console.log(mname + ' allocating ' + toAllocate);
+				buffer[i] = new Buffer(toAllocate * 1024 * 1024);
+				buffer[i].fill('X');
 			}
-			console.log(mname + ' allocating ' + toAllocate);
-			buffer[i] = new buf(toAllocate * 1024 * 1024);
-			buffer[i].fill('X');
 		}
+	}
+	catch (e) {
+		console.log(mname + ' ERROR ', e);
 	}
 };
 
 
 exports.release_ram = function () {
 	if (buffer) {
-		console.log(mname + ': releasing allocation reference...');
 		buffer.forEach(function (b) {
+			console.log(mname + ': releasing allocation reference...');
 			b = null;
 		});
 		buffer = null;
 	} else {
-		console.log(mname + ': nothing allocated');
+		console.log(mname + ': no ram allocated');
 	}
 };
 
 
 exports.eat_cpu = function (callback) {
 	//process.env.UV_THREADPOOL_SIZE = Math.ceil(Math.max(4, require('os').cpus().length * 1.5));
-	stop_eating_cpu = false;
-	console.log(mname + ': eating cpu');
-	while (true !== stop_eating_cpu) {
-		fibonacci(43);
+	//process.nextTick(function () {
+	//	stop_eating_cpu = false;
+	//	console.log(mname + ': eating cpu');
+	//	for (var i = 0; i < 2; i++) {
+	//		fibonacci(43);
+	//		if (1 === i) { i = -1; }
+	//		if (true === stop_eating_cpu) { break; }
+	//	}
+	//	callback(null, mname + ': eating cpu stopped');
+	//});
+	for (var i = 0; i < 5; i++) {
+		fibonacciAsync(5, i, function (res) {
+			callback(null, res);
+		
+		});
 	}
-	console.log(mname + ': eating cpu stopped');
-	callback(null, 'eating cpu stopped');
 };
 
 
-exports.stop_cpu = function (){
+exports.stop_cpu = function () {
+	console.log(mname + ': cpu stop signal received');
 	stop_eating_cpu = true;
 }
 
-function fibonacci(n) {
-	if (true === stop_eating_cpu) {
-		return 1;
+
+function fibonacciAsync(n, i, done) {
+	console.log(mname + ': stop_eating_cpu '+ i, stop_eating_cpu);
+	if (true === stop_eating_cpu) { return; }
+	
+	if (n == 1 || n == 2) {
+		done(1);
 	}
+	else {
+		setImmediate(function () {
+			fibonacciAsync(n - 1, i , function (val1) {
+				setImmediate(function () {
+					fibonacciAsync(n - 2, i, function (val2) {
+						done(val1 + val2);
+					});
+				});
+			});
+		});
+	}
+}
+
+
+function fibonacciAsyncORIG(n, done) {
+	if (true === stop_eating_cpu) { done(1); }
+	
+	if (n == 1 || n == 2) {
+		done(1);
+	}
+	else {
+		process.nextTick(function () {
+			fibonacciAsync(n - 1 , function (val1) {
+				process.nextTick(function () {
+					fibonacciAsync(n - 2, function (val2) {
+						done(val1 + val2);
+					});
+				});
+			});
+		});
+	}
+}
+
+
+function fibonacci(n) {
+	if (true === stop_eating_cpu) { return 1; }
+	
 	if (n < 2) {
 		return 1;
 	} else {
--- node-crusher/test/test-ram.js
+++ node-crusher/test/test-ram.js
@@ -6,17 +6,10 @@ crusher.eat_ram(1023);
 crusher.eat_ram(1025);
 
 setTimeout(function () {
-	crusher.eat_ram(2500);
+	crusher.eat_ram(500);
 	setTimeout(function () {
 		crusher.eat_ram(300);
-		console.log(mname + ': about to start cpu');
-		crusher.eat_cpu(function (err, msg) { console.log(mname + ': ' + msg); });
-		console.log(mname + ': started cpu');
-		setTimeout(function () {
-			console.log(mname + ': about to stop cpu');
-			crusher.stop_cpu();
-			console.log(mname + ': finished');
-		}, 3 * 1000);
+		console.log(mname + ': finished');
 	}, 3 * 1000);
 }, 3 * 1000);
 
